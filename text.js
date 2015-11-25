018276c2b0efd2638956a300adc8951c50a64a06
Justin Jones <justin@jtjones.codes>
2015-11-24 21:40:16 -0600

    Added resource files. Other minor changes.

--- text.js
+++ text.js
@@ -1,4 +1,14 @@
 // this is the javascript file to manipulate the text
-function init() {
-	console.log('V 0.0.0.1');
-}
+$( document ).ready ( function () {
+	console.log('V 0.0.0.7');
+
+	$(".styleBtn").click(function () {
+		// function runs whenever any of the style buttons are clicked
+		// will have to call other fuctions to modify the text
+		// $(this) is the jquery object referencing the clicked element (buttons as divs in this case)
+		console.log($(this).attr('value'));
+		// use jquery .wrap to wrap either tags or styled spans around selected text
+		$( this ).toggleClass('activeBtn');
+	});
+
+});
