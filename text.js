e52e1be4018c4a289839a9c88dbf9fa4b53c50bd
Justin Jones <justin@jtjones.codes>
2015-11-24 23:14:50 -0600

    Tried to run using Electron; it worked at first, then hit weird issues. Made other minor progressive changes.

--- text.js
+++ text.js
@@ -6,7 +6,7 @@ $( document ).ready ( function () {
 		// function runs whenever any of the style buttons are clicked
 		// will have to call other fuctions to modify the text
 		// $(this) is the jquery object referencing the clicked element (buttons as divs in this case)
-		console.log($(this).attr('value'));
+		console.log($(this).attr('mod'));
 		// use jquery .wrap to wrap either tags or styled spans around selected text
 		$( this ).toggleClass('activeBtn');
 	});
